172ed0cd21c5bece17328622ab1c248aa139c00d
Parth Agarwal <parthagarwal57@gmail.com>
2025-08-28 10:34:06 +0530

    Third Commit

--- tests/test_op.py
+++ tests/test_op.py
@@ -4,7 +4,11 @@ from src.maths_op import add, sub
 def test_add():
     assert add(2,3) == 5
     assert add(-1,1) == 0
+    assert add(10,15) == 25
+    assert add(1,1) == 2
     
 def test_sub():
     assert sub(5,3) == 2
     assert sub(-1,1) == -2
+    assert sub(2,1) == 1
+    assert sub(5,0) == 5
